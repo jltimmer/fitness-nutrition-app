90aa09b0ec8387209efc375264fa7613cd977937
Janna Timmer <jltimmer@users.noreply.github.com>
2023-08-30 11:18:05 -0400

    Simplify view and add delete endpoint

--- fitnessapp/fitness/views.py
+++ fitnessapp/fitness/views.py
@@ -1,21 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 from .models import Exercise
 from .serializers import ExerciseSerializer
 
-@api_view(['GET', 'POST'])
-def index(request):
-	if request.method == 'GET':
-		data = Exercise.objects.all()
-		serializer = ExerciseSerializer(data, context={'request': request}, many=True)
-		return Response(serializer.data)
 
-	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = ExerciseSerializer(data=data, context={'request': request})
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=201)
-		return Response(serializer.erros, status=400)
+class ExercisesViewSet(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+    allowed_methods = ["GET", "POST", "DELETE"]
