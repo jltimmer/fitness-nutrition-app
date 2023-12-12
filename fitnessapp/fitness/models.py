41f456080f016f6f16d27334ca08a3d52524ceb9
Janna Timmer <jltimmer@users.noreply.github.com>
2023-12-11 19:30:17 -0500

    Create food model

--- fitnessapp/fitness/models.py
+++ fitnessapp/fitness/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
-class Exercise(models.Model):
-	name = models.CharField(max_length=30)
-	notes = models.CharField(max_length=200)
 
-	def __str__(self):
-		return f"{self.name}: {self.notes}"
+class Exercise(models.Model):
+    name = models.CharField(max_length=30)
+    notes = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}: {self.notes}"
+
 
 class CardioExercise(Exercise):
     seconds = models.PositiveIntegerField(default=0)
-    
+
+
 class StrengthExercise(Exercise):
     sets = models.PositiveIntegerField(default=0)
     reps = models.PositiveIntegerField(default=0)
     weight = models.PositiveIntegerField(default=0)
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=30)
+    calories = models.IntegerField(default=0)
+    notes = models.CharField(max_length=200)
--- fitnessapp/fitness/serializers.py
+++ fitnessapp/fitness/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Exercise
+from .models import Exercise, Food
+
 
 class ExerciseSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Exercise
-		fields = ('id', 'name', 'notes')
+    class Meta:
+        model = Exercise
+        fields = ("id", "name", "notes")
+
+
+class FoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Food
+        fields = ("id", "name", "calories", "notes")
